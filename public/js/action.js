718e033746631e19ee0fde471bf37eb677cf054e
Melki <melchior.toldi@gmail.com>
2013-10-16 14:48:46 +0200

    context fixed ?

--- public/js/action.js
+++ public/js/action.js
@@ -8,7 +8,9 @@ $(document).ready(function() {
   var oneClick=0;
   var canvas = $("#canvas");
   var cursorX, cursorY,oldX,oldY;
-  var context = canvas[0].getContext('2d');
+  var context = new Array();
+  context[0] = canvas[0].getContext('2d');
+  context[1] = canvas[0].getContext('2d');
   //context.canvas.width  = window.innerWidth;
   //context.canvas.height = window.innerHeight;
   var socket = io.connect();
@@ -16,8 +18,8 @@ $(document).ready(function() {
   var oldPostionX=new Array();
   var oldPostionY=new Array();
   // Trait arrondi :
-  context.lineJoin = 'round';
-  context.lineCap = 'round';
+  context[0].lineJoin = 'round';
+  context[0].lineCap = 'round';
     
 
     socket.on('reset', function (data)
@@ -37,26 +39,26 @@ $(document).ready(function() {
     { 
       if(data[6]===0)
       {
-         context.beginPath();
-         context.arc(data[2], data[3], data[1]/2, 0, 2 * Math.PI, false);
-         context.fillStyle = data[0];
-         context.fill();
+         context[1].beginPath();
+         context[1].arc(data[2], data[3], data[1]/2, 0, 2 * Math.PI, false);
+         context[1].fillStyle = data[0];
+         context[1].fill(); 
          
       }
       if(!data[4]){oldPostionX[data[5]]=null;}
       if(!oldPostionX[data[5]])
       {
-        context.beginPath();  
+        context[1].beginPath();  
         oldPostionX[data[5]]=data[2];
         oldPostionX[data[5]]=data[3];
       }
       else
       {
-        context.moveTo(oldPostionX[data[5]], oldPostionY[data[5]]);
-        context.lineTo(data[2], data[3]);
-        context.strokeStyle = data[0];
-        context.lineWidth = data[1];
-        context.stroke();
+        context[1].moveTo(oldPostionX[data[5]], oldPostionY[data[5]]);
+        context[1].lineTo(data[2], data[3]);
+        context[1].strokeStyle = data[0];
+        context[1].lineWidth = data[1];
+        context[1].stroke();
         oldPostionX[data[5]]=data[2];
         oldPostionX[data[5]]=data[3];
 
@@ -70,26 +72,26 @@ $(document).ready(function() {
       {
     if(data[i][6]===0)
       {
-         context.beginPath();
-         context.arc(data[i][2], data[i][3], data[i][1]/2, 0, 2 * Math.PI, false);
-         context.fillStyle = data[i][0];
-         context.fill();
+         context[1].beginPath();
+         context[1].arc(data[i][2], data[i][3], data[i][1]/2, 0, 2 * Math.PI, false);
+         context[1].fillStyle = data[i][0];
+         context[1].fill();
          
       }
       if(!data[i][4]){oldPostionX[data[i][5]]=null;}
       if(!oldPostionX[data[i][5]])
       {
-        context.beginPath();  
+        context[1].beginPath();  
         oldPostionX[data[i][5]]=data[i][2];
         oldPostionX[data[i][5]]=data[i][3];
       }
       else
       {
-        context.moveTo(oldPostionX[data[i][5]], oldPostionY[data[i][5]]);
-        context.lineTo(data[i][2], data[i][3]);
-        context.strokeStyle = data[i][0];
-        context.lineWidth = data[i][1];
-        context.stroke();
+        context[1].moveTo(oldPostionX[data[i][5]], oldPostionY[data[i][5]]);
+        context[1].lineTo(data[i][2], data[i][3]);
+        context[1].strokeStyle = data[i][0];
+        context[1].lineWidth = data[i][1];
+        context[1].stroke();
         oldPostionX[data[i][5]]=data[i][2];
         oldPostionX[data[i][5]]=data[i][3];
 
@@ -118,7 +120,7 @@ $(document).ready(function() {
   // Click souris enfoncé sur le canvas, je dessine :
   canvas.mousedown(function(e) {
     oneClick=0;
-    context.closePath();
+    context[0].closePath();
     painting = true;
     var targ;
     if (!e)
@@ -141,13 +143,13 @@ $(document).ready(function() {
   $(this).mouseup(function() {
      if(painting===true && oneClick===0)
      {
-      context.closePath();
+      context[0].closePath();
       sendInfo(0);
-      context.beginPath();
-      context.arc(cursorX, cursorY, width_brush/2, 0, 2 * Math.PI, false);
-      context.fillStyle = color;
-      context.fill();
-      context.closePath();
+      context[0].beginPath();
+      context[0].arc(cursorX, cursorY, width_brush/2, 0, 2 * Math.PI, false);
+      context[0].fillStyle = color;
+      context[0].fill();
+      context[0].closePath();
      
     }
     painting = false;
@@ -176,7 +178,7 @@ $(document).ready(function() {
       cursorX = (e.pageX -  $(targ).offset().left) - 1;
       cursorY = (e.pageY -  $(targ).offset().top) - 1;
       // Dessine une ligne :
-      context.closePath();
+      context[0].closePath();
       drawLine();
     }
   });
@@ -188,25 +190,25 @@ $(document).ready(function() {
     // Si c'est le début, j'initialise
     if (!started) {
       // Je place mon curseur pour la première fois :
-      context.beginPath();
-      context.moveTo(cursorX, cursorY);
+      context[0].beginPath();
+      context[0].moveTo(cursorX, cursorY);
       started = true;
 
     }
     // Sinon je dessine
     else {
-      context.beginPath();
-      context.moveTo(oldX, oldY);
-      context.lineTo(cursorX, cursorY);
-      context.strokeStyle = color;
-      context.lineWidth = width_brush;
-      context.stroke();
+      context[0].beginPath();
+      context[0].moveTo(oldX, oldY);
+      context[0].lineTo(cursorX, cursorY);
+      context[0].strokeStyle = color;
+      context[0].lineWidth = width_brush;
+      context[0].stroke();
     }
   }
   
   // Clear du Canvas :
   function clear_canvas() {
-    context.clearRect(0,0, canvas.width(), canvas.height());
+    context[0].clearRect(0,0, canvas.width(), canvas.height());
   }
   
   // Pour chaque carré de couleur :
