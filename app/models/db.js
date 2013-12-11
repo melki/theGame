7bbe98ee8db3a732cd9de1464a44456a342e4af2
Melki <melchior.toldi@gmail.com>
2013-12-11 16:41:23 +0100

    mongodb connection heroku

--- app/models/db.js
+++ app/models/db.js
@@ -8,4 +8,15 @@
 
   mongoose.model('scoreSchema', scoreSchema)
 
-  mongoose.connect('mongodb://localhost/thetest#1');
+  var uristring =
+	process.env.MONGOLAB_URI ||
+	process.env.MONGOHQ_URL ||
+	'mongodb://localhost/thetest#1';
+
+  mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
